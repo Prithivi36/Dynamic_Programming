a15f1c6a40edd204cf4d1079dc175613f95543e1
Prithivi36 <prithivipappian@gmail.com>
2025-01-01 14:25:46 +0530

    Finding All possible Subsets

---
+++ src/Backtracking/FindingSubSets.java
@@ -0,0 +1,16 @@
+package Backtracking;
+
+public class FindingSubSets {
+    public static void main(String[] args) {
+        String str="abc";
+        subSets("",str,0,0);
+    }
+    public static void subSets(String str,String inp,int i,int j){
+        if(i== inp.length()){
+            System.out.println(str);
+            return;
+        }
+        subSets(str+inp.charAt(i),inp,i+1,j+1);
+        subSets(str,inp,i+1,j);
+    }
+}
